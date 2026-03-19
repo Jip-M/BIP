325b95e3cc49e04664d2f75c633c3159b8ad6b18
Jip-M <s.j.mijnheer@student.rug.nl>
2026-03-19 17:10:31 +0100

    merged columns, future height as 'future_height'

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,14 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # filepath
-fn = "/wsl.localhost/Ubuntu/home/jip/nfi_germany_treedata/bwi_tree.csv"
+fn = "/home/jip/nfi_germany_treedata/bwi_tree.csv"
 
 df = pd.read_csv(fn, sep=";", encoding="iso-8859-1")
 
+
 def filter_rows(df):
+    """After filtering, 208544 treeID's will remain twice: 1 time around 2000, 1 time around 2010"""
     # 1. Convert year to integer
     df["year"] = pd.to_numeric(df["year"], errors="coerce").astype(int)
 
@@ -32,3 +35,50 @@ def filter_rows(df):
     print(f"Original trees: {df['tree_ID'].nunique()}")
     print(f"Filtered trees: {df_filtered['tree_ID'].nunique()}")
 
+    return df_filtered
+
+
+# dont need this. Plot is already saved.
+def plot_barplot(df_filtered):
+    year_counts = df_filtered["year"].value_counts().sort_index()
+
+    plt.bar(year_counts.index, year_counts.values)
+    plt.xlabel("Year")
+    plt.ylabel("Number of measurements")
+    plt.title("Filtered tree Measurements per year")
+
+    plt.show()
+
+
+def merge_columns(df_filtered):
+    """splits 2000 / 2010 trees, merges 2010 tree heights with 2000's trees as 'future_height', and reorders columns"""
+
+    # 1. Create the Feature Set (Train) from the 1995-2005 window
+    train_df = df_filtered[df_filtered["year"] < 2005]
+
+    # # 2. Create the Label Set from the 2010+ window
+    label_df = df_filtered[df_filtered["year"] > 2005][["tree_ID", "height"]]
+
+    # 1. merge columns
+    label_df = label_df.rename(columns={"height": "future_height"})
+    train_df = pd.merge(train_df, label_df, on="tree_ID")
+    print(f"Final training rows: {len(train_df)}")
+
+    # reorder columns
+    cols = list(train_df.columns)
+    leading_cols = ["tree_ID", "height", "future_height"]
+    remaining_cols = [c for c in cols if c not in leading_cols]
+    new_order = leading_cols + remaining_cols
+    train_df = train_df[new_order]
+
+    print("Columns successfully merged and reordered:\n")
+    print(train_df.head())
+
+    return train_df
+
+
+df_filtered = filter_rows(df)
+train_df = merge_columns(df_filtered)
+print(train_df.head())
+
+train_df.to_csv("train.csv", index=False)
