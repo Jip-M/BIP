3403f34585169df96324b6d63fb92de8ca2e9554
hlovisa4 <lovisahamback@gmail.com>
2026-03-19 18:16:33 +0100

    upd xgboost

--- xgboost_run.py
+++ xgboost_run.py
@@ -12,13 +12,24 @@ import numpy as np
 file_path = "/mnt/c/Users/Lovisa/Downloads/nfi_germany_treedata/bwi_tree.csv"
 data = pd.read_csv(file_path, sep = ";", decimal=",")
 data["plot_hydrol_class"] = data["plot_hydrol_class"].astype(str)
-data = data.sample(n=10000, random_state=42)
+data = data.sample(n=20000, random_state=42)
 #print(data.columns.values)
+
+cat_cols = data.select_dtypes(include=['object', 'category']).columns
+
+
+for col in cat_cols:
+    data[col] = data[col].astype("category")
+
+for col in data.select_dtypes(include=["float64"]).columns:
+    data[col] = pd.to_numeric(data[col], downcast="float")
+
+for col in data.select_dtypes(include=["int64"]).columns:
+    data[col] = pd.to_numeric(data[col], downcast="integer")
+
 y = data["dbh"]
 X = data.drop(columns=["dbh"])
-print(len(X))
 X = X.dropna(axis=1, how="all")
-print(len(X))
 mask = y.notna()
 X = X[mask]
 y = y[mask]
@@ -31,11 +42,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
 
-cat_cols = data.select_dtypes(include=['object', 'category']).columns
 
 for col in cat_cols:
-    X_train[col] = X_train[col].astype("category")
-    X_test[col] = X_test[col].astype("category")
     X_test[col] = X_test[col].cat.set_categories(X_train[col].cat.categories)
 
 model = xgb.XGBRegressor(objective='reg:squarederror', enable_categorical=True, random_state=42)
